95dab0c5f1a2570c2458be705627ab58d7f03c28
Gaetan Godin <gaetgodi@gmail.com>
2026-08-15 19:11:39 +0000

    Initial divi-sales-child scaffold

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function divi_sales_child_enqueue_styles() {
+    wp_enqueue_style( "divi-parent-style", get_template_directory_uri() . "/style.css" );
+    wp_enqueue_style( "divi-sales-child-style",
+        get_stylesheet_directory_uri() . "/style.css",
+        array( "divi-parent-style" ),
+        wp_get_theme()->get("Version")
+    );
+}
+add_action( "wp_enqueue_scripts", "divi_sales_child_enqueue_styles" );
