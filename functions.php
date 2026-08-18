7d302c41a1c26950c5d8a9351f0b343b4b478444
Gaetan Godin <gaetgodi@gmail.com>
2026-08-18 18:27:50 +0000

    Add design tokens CSS and enqueue in functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 function divi_sales_child_enqueue_styles() {
     wp_enqueue_style( "divi-parent-style", get_template_directory_uri() . "/style.css" );
+    wp_enqueue_style( "godindev-tokens",
+        get_stylesheet_directory_uri() . "/godindev-tokens.css",
+        array( "divi-parent-style" ),
+        wp_get_theme()->get("Version")
+    );
     wp_enqueue_style( "divi-sales-child-style",
         get_stylesheet_directory_uri() . "/style.css",
-        array( "divi-parent-style" ),
+        array( "divi-parent-style", "godindev-tokens" ),
         wp_get_theme()->get("Version")
     );
 }
